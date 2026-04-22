44c5ccb514d2b73fe5944506a337b05679bf006a
TranQHUng <tqhungktpm2211072@student.ctuet.edu.vn>
2026-04-23 00:55:14 +0700

    Initial Commit

---
+++ todoTests/todoTests.swift
@@ -0,0 +1,19 @@
+//
+//  todoTests.swift
+//  todoTests
+//
+//  Created by Trần Quang Hưng on 23/4/26.
+//
+
+import Testing
+@testable import todo
+
+struct todoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
